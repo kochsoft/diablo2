084743045341ff81fdf54f4b7928112b33201d90
Markus-Hermann Koch <kochmn@localhost.localdomain>
2025-03-16 21:57:30 +0100

    research(runeword magic): Careful editing led to a "magic" runeword.
    Able to create a hat 'examples/tailored_magic_runeword_LORE.cube'.
    It has the runeword LORE special properties as a magical enhancement.
    No sockets. No runes.

--- examples/fix.sh
+++ examples/fix.sh
@@ -5,5 +5,6 @@ cp Charonson.d2s.backup Charonson.d2s
 cp Lancelot.d2s.backup Lancelot.d2s
 cp Schnupsor.d2s.backup Schnupsor.d2s
 cp Ordu.d2s.backup Ordu.d2s
+cp Otto.d2s.backup Otto.d2s
 echo "Cleant house!"
 
